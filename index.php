40dd433eff4727b923309e22f8eab68359701a80
Todor Tsekov <t.v.tsekov@gmail.com>
2020-11-01 19:31:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Todor Tsekov &mdash; Project Portfolio Website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="This webiste will give you an overview of my own finished projects to this date." />
+	<meta name="keywords" content=" html5, css3, mobile first, responsive" />
+	<meta name="author" content="Todor Tsekov" />
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Playfair+Display" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+<?php  ?>
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="top-menu">
+				<div class="row">
+					<div class="col-sm-2">
+						<div id="fh5co-logo"><a href="index.html"><img src="./images/T-Logo.png" alt=""></a></div>
+					</div>
+					<div class="col-sm-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.html">Portfolio</a></li>
+							<li><a href="single.html">Resume</a></li>
+							<li><a href="single.html">About Me</a></li>
+							<!-- <li class="has-dropdown"><a href="single.html">About Me</a>
+								<ul class="dropdown">
+									<li><a href="#">Infrastructure</a></li>
+									<li><a href="#">Residential</a></li>
+									<li><a href="#">Environmental</a></li>
+									<li><a href="#">Megabuilders</a></li>
+								</ul>
+							</li> -->
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div id="fh5co-intro">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 col-md-pull-2">
+					<h2>Welcome! ... To my site. <span><em>My name is</em> <a href="http://freehtml5.co">Todor.</a></span></h2>
+				</div>
+			</div>
+		</div>
+		<aside id="fh5co-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/img_bg_1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-xs-8 col-md-offset-1 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><a href="#">Best showcase for architecture</a></h1>
+										<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-xs-8 col-md-offset-1 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><a href="#">Best showcase for architecture</a></h1>
+										<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-xs-8 col-md-offset-1 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><a href="#">Best showcase for architecture</a></h1>
+										<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-xs-8 col-md-offset-1 slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1><a href="#">Best showcase for architecture</a></h1>
+										<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>	   	
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="fh5co-portfolio">
+			<div class="row nopadding">
+				<div class="col-md-6 padding-right">
+					<div class="row">
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-5.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+						
+					</div>
+				</div>
+
+				<div class="col-md-6 padding-left">
+					<div class="row">
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-6.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+
+						<div class="col-md-12 animate-box">
+							<a href="single.html" class="portfolio-grid">
+								<img src="images/portfolio-7.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+								<div class="desc">
+									<h3>Dublin Arena Architect Project</h3>
+									<span>Building, Arena</span>
+								</div>
+							</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- END container -->
+
+	<div class="container">
+		<footer id="fh5co-footer" role="contentinfo">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2020 Todor's portfolio website. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">Todor Tsekov</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<!-- <li><a href="#"><i class="icon-mail4"></i></a></li> -->
+						</ul>
+					</p>
+				</div>
+			</div>
+		</footer>
+	</div><!-- END container -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Sticky Kit -->
+	<script src="js/sticky-kit.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
