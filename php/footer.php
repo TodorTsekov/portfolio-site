841c5bbde54800143f87cfea382f79078f9ae221
Todor Tsekov <t.v.tsekov@gmail.com>
2020-11-02 10:03:08 +0100

    PHP-Managing redundant code into small php files.

---
+++ php/footer.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <footer id="fh5co-footer" role="contentinfo">
+        <div class="row copyright">
+            <div class="col-md-12 text-center">
+                <p>
+                    <small class="block">&copy; 2020 Todor's portfolio website. All Rights Reserved.</small>
+                    <small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">Todor Tsekov</a></small>
+                </p>
+                <p>
+                    <ul class="fh5co-social-icons">
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <!-- <li><a href="#"><i class="icon-mail4"></i></a></li> -->
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </footer>
+</div><!-- END container -->
---
+++ php/navigation.php
@@ -0,0 +1,28 @@
+<nav class="fh5co-nav" role="navigation">
+    <div class="container">
+        <div class="top-menu">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div id="fh5co-logo"><a href="index.php"><img src="./images/T-Logo.png" alt=""></a></div>
+                </div>
+                <div class="col-sm-10 text-right menu-1">
+                    <ul>
+                        <li class="active"><a href="index.php">Portfolio</a></li>
+                        <li><a href="single.php">Resume</a></li>
+                        <li><a href="single.php">About Me</a></li>
+                        <!-- <li class="has-dropdown"><a href="single.html">About Me</a>
+								<ul class="dropdown">
+									<li><a href="#">Infrastructure</a></li>
+									<li><a href="#">Residential</a></li>
+									<li><a href="#">Environmental</a></li>
+									<li><a href="#">Megabuilders</a></li>
+								</ul>
+							</li> -->
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</nav>
